2816ade6e3f7004691115f98507111d6d2e242ac
burnplusplus <138609506+burnplusplus@users.noreply.github.com>
2023-07-28 10:02:51 -0300

    Add files via upload

---
+++ const/contractAddresses.ts
@@ -0,0 +1,15 @@
+import {  } from "@thirdweb-dev/chains";
+export const NETWORK = "binance";
+
+
+export const NFT_COLLECTION_ADDRESS = "0xE2B12125dCa462E8A28b61Cd2dA0D556a2B755a6"; //NFT BNB
+export const tokenContractAddress= "0xdA2c0CDf7d764F8C587380CAdF7129E5eCb7Efb7";
+
+export const nftDropContractAddress ="0xE2B12125dCa462E8A28b61Cd2dA0D556a2B755a6";
+export const stakingContractAddress ="0xE2B12125dCa462E8A28b61Cd2dA0D556a2B755a6";
+// (Optional) Set up the URL of where users can view transactions on
+// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
+export const ETHERSCAN_URL = "https://bscscan.com";
+
+
+
